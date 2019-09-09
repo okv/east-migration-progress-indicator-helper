686d229200ff886fc1769a2763f8a59f5aca7598
oleg <oleg.korobenko@gmail.com>
2019-09-09 15:54:20 +0300

    add progress logger

--- test/createProgressIndicator.js
+++ test/createProgressIndicator.js
@@ -4,6 +4,7 @@ const tap = require('tap');
 const expect = require('expect.js');
 const createProgressIndicator = require('../lib/createProgressIndicator');
 const ProgressBar = require('../lib/ProgressBar');
+const ProgressLogger = require('../lib/ProgressLogger');
 
 tap.mochaGlobals();
 
@@ -31,6 +32,42 @@ describe('createProgressIndicator', () => {
 
 		it('progress bar should tick without error', () => {
 			indicator.tick(100);
+		});
+	});
+
+	describe('with not tty', () => {
+		let indicator;
+
+		before(() => {
+			process.stdout.isTTY = false;
+		});
+
+		after(() => {
+			process.stdout.isTTY = isTTY;
+		});
+
+		it('should be done without error', () => {
+			indicator = createProgressIndicator({total: 100});
+		});
+
+		it('should create progress logger', () => {
+			expect(indicator).a(ProgressLogger);
+		});
+
+		it('progress logger should tick 1 without error', () => {
+			indicator.tick();
+		});
+
+		it('progress logger should tick 1 without error', () => {
+			indicator.tick(1);
+		});
+
+		it('progress logger should tick 9 without error', () => {
+			indicator.tick(9);
+		});
+
+		it('progress logger should tick 89 without error', () => {
+			indicator.tick(89);
 		});
 	});
 });
